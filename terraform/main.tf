99284790342e53b13ffc226ee0649483bad59ec7
Nafeesp2001 <151185450+Nafeesp2001@users.noreply.github.com>
2025-02-16 16:42:19 +0530

    Add files via upload

--- terraform/main.tf
+++ terraform/main.tf
@@ -131,7 +131,7 @@ resource "aws_iam_role_policy_attachment" "lambda_glue_policy" {
 
 # 8. Create Lambda Function
 resource "aws_lambda_function" "etl_lambda" {
-  filename = "/var/jenkins_home/workspace/demo-cloud-pipeline/lambda_function.zip"
+  filename = "/var/jenkins_home/workspace/domo-cloud-pipeline/lambda_function.zip"
   function_name    = "etl_lambda"
   role            = aws_iam_role.lambda_role.arn
   handler         = "lambda_function.lambda_handler"
