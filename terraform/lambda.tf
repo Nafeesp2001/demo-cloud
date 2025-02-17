042dc5872e4361ecf6214432cbfeec65fa44c357
Nafeesp2001 <151185450+Nafeesp2001@users.noreply.github.com>
2025-02-17 11:00:04 +0530

    Add files via upload

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -2,13 +2,14 @@ resource "aws_lambda_function" "etl_lambda" {
   function_name    = "etl_lambda"
   role            = aws_iam_role.lambda_role.arn
   package_type    = "Zip"
-  image_uri       = "var/jenkins_home/workspace/domo-cloud-pipeline/lambda_function.zip"  # 👈 Accessing ECR repo from main.tf
+  filename        = "/var/jenkins_home/workspace/domo-cloud-pipeline/lambda_function.zip"  # ✅ Correct file path
+  handler         = "etl.lambda_handler"  # ✅ Correct handler for Python file
+  runtime         = "python3.9"  # ✅ Required for ZIP-based Lambda
 
   memory_size     = 512
-  timeout        = 60
+  timeout         = 60
 
   depends_on = [
-    aws_ecr_repository.etl_repo,  # 👈 Ensures Lambda runs only after ECR is ready
     aws_s3_bucket.etl_bucket,
     aws_db_instance.etl_rds
   ]
